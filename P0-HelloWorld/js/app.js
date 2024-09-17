ae8f7c2b3c43a9dde6d52d6ab78c2b143ae74e77
SofZde <sofzde138@ihmeducation.se>
2024-09-17 08:10:03 +0200

    testing button

---
+++ P0-HelloWorld/js/app.js
@@ -0,0 +1,9 @@
+/* Document: calls for referens from documents
+GEt element : returns element (found "button" ( btnConvert) that I made in index.html)
+Add event: calls for an action to happen --> click is that action that I want.
+Funktion: run a block of code (when the click activates).
+*/
+document.getElementById( "btnConvert").addEventListener("mouseover", function(){
+  alert("click" ) // Depending on the event (addevent) the oytcome changes on the website.
+});
+
