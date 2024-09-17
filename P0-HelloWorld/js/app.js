713c171bab875f97540083b8983e1326f3833839
SofZde <sofzde138@ihmeducation.se>
2024-09-17 20:06:42 +0200

    test

--- P0-HelloWorld/js/app.js
+++ P0-HelloWorld/js/app.js
@@ -1,26 +1,8 @@
-/* Document: calls for referens from documents
-GEt element : returns element (found "button" ( btnConvert) that I made in index.html)
-Add event: calls for an action to happen --> click is that action that I want.
-Funktion: run a block of code (when the click activates).
-*/
-// create array 1, 2 , 3 (sten sax påse)
-//function that generates random array?
-let answer;
-answer = prompt( "sten, sax eller påse?");
-alert("motståndaren väljer" + RandomArray(1-3)) ; // sten sax eller påse
+// array of string, våra val
+const val= [ "sten, sax, påse"];
 
-switch (answer)
-case "sten"
-{
+// hhämta element från html indexfil
+const spelareDisplay= document.getElementById(" spelareDisplay");
+const datorDisplay= document.getElementById(" datorDisplay");
+const resultatDisplay= document.getElementById(" resultatDisplay");
 
-}
-
-case "sax"
-{
-
-}
-
-case "påse";
-{
-
-}
